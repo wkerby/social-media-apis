8e79eaafceb8d639b7988d03170087f2a83ea888
William Kerby <w.kerby99@gmail.com>
2023-05-03 17:24:03 -0400

    begin Thought schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,8 @@
-//require validateEmail function
-const validateEmail = require('../utils/helpers');
-
 //require in mongoose
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true, trim: true},
+    thoughtText: { type: String, required: true, min:1, max:280},
     email: { type: String, unique: true, required: true, trim: true, validate: [validateEmail, "Please provide a valid email address"]}, 
     thoughts: { type: Array, }
 })
-
