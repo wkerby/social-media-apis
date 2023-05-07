f8a84a52c047b9f5e358a3c409b4899c37a8f356
William Kerby <w.kerby99@gmail.com>
2023-05-07 12:17:10 -0400

    begin get all users route and create userRoutes.js file

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+
+//bring in the User and Thought models
+const {User, Thought} = require("../../models");
+
+//get all users
+router.get('/', async(req, res) => {
+    try {
+        User.find({}, (err, result));
+        res.status(200).json(result);
+    }
+
+    catch (err) {
+        console.log('Oops! Something went wrong!')
+        res.status(500).json(err);
+    }
+
+})
+
