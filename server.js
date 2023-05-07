5b35d93dd700841136ea3ae8c2a01b8255c78bd3
William Kerby <w.kerby99@gmail.com>
2023-05-07 15:48:05 -0400

    add /users endpoint and modify server.js

--- server.js
+++ server.js
@@ -11,10 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-});
 
 db.once('open', () => {
     app.listen(PORT, () => {
