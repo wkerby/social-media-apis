3899f125ca37fcc9dcf82a4542456081484d949f
William Kerby <w.kerby99@gmail.com>
2023-05-07 11:40:13 -0400

    ensure that reactions field in Thought model is an array of all reaction subdocuments associated with thought

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,1 +1,23 @@
-const mongoose = requrie('mongoose');
+//require in mongoose
+const { ObjectId } = require('mongodb');
+const mongoose = require('mongoose');
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: { type: ObjectId, default: ObjectId},
+    
+    reactionBody: { type: String, required: true, max: 280 },
+
+    username: { type: String, required: true}
+},
+{
+    timestamps:true //automatically creates fields for created and updated time stamps
+});
+
+const getCurrDate = () => { //create a function that retrieves the created date of the thought
+    return this.createdAt;
+}
+
+reactionSchema.virtual('dateCreated').get(getCurrDate); //use function in virtual 
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+module.exports = Reaction;
--- models/Thought.js
+++ models/Thought.js
@@ -1,16 +1,16 @@
 //require User model
 const User = require("./User");
 
-//create var for current date and time
-const date = new Date();
-const today = date.toLocaleString();
-//require in mongoose
+//require Reaction schema
+const Reaction = require("./Reaction");
+
 const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, min:1, max:280},
     // createdAt: { type: Date, default: today },
     username: { type: String, required: true, unique: true, ref: 'User'},
+    reactions: [Reaction.schema]
 },
 {
     timestamps:true //automatically creates fields for created and updated time stamps
