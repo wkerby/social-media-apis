1aecc9eead33165129d018eabbb13ee266e6e96a
William Kerby <w.kerby99@gmail.com>
2023-05-07 10:00:27 -0400

    add virtual for friendcount in user model

--- models/User.js
+++ models/User.js
@@ -10,7 +10,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true, trim: true},
     email: { type: String, unique: true, required: true, trim: true, validate: [validateEmail, "Please provide a valid email address"]}, 
-    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought'}]
+    thoughts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Thought'}],
+    friends: [{type:mongoose.Schema.Types.ObjectId, ref: 'User'}]
+})
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
 })
 
 const User = mongoose.model('User', userSchema);
