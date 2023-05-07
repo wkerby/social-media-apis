cc965feeef341f354fe0cfd4f2a80f81d4372a79
William Kerby <w.kerby99@gmail.com>
2023-05-07 10:24:08 -0400

    create a virtual to return the create date of thought in thought model

--- models/Thought.js
+++ models/Thought.js
@@ -9,9 +9,18 @@ const mongoose = require('mongoose');
 
 const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, min:1, max:280},
-    createdAt: { type: Date, default: today },
+    // createdAt: { type: Date, default: today },
     username: { type: String, required: true, unique: true, ref: 'User'},
-})
+},
+{
+    timestamps:true //automatically creates fields for created and updated time stamps
+});
+
+const getCurrDate = () => { //create a function that retrieves the created date of the thought
+    return this.createdAt;
+}
+
+thoughtSchema.virtual('dateCreated').get(getCurrDate); //use function in virtual 
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
