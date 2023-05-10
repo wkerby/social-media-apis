24e494d68151d73e6670e91a09a452241778a531
William Kerby <w.kerby99@gmail.com>
2023-05-10 06:38:06 -0400

    add route to retrieve all thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,3 +4,14 @@ const router = require('express').Router();
 const { Thought } = require("../../models");
 
 //get all thoughts
+router.get('/', async (req,res) => {
+    console.log("Attempting to get all users")
+    try {
+        const findUsers = await Thought.find({}).populate({path:'reactions',select:'-__v'}).select('-__v');
+        res.status(200).json(findUsers);
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
